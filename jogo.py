faded46d3161860dea5e63b03e38c61eff4e2318
arthurpansera <163167633+arthurpansera@users.noreply.github.com>
2024-11-15 15:25:06 -0300

    Trabalho Final
    Resolução de Problemas com Lógica Matemática

--- jogo.py
+++ jogo.py
@@ -6,6 +6,7 @@ preto = (0, 0, 0)
 verde = (34, 177, 76)
 amarelo = (255, 223, 0)
 vermelho = (255, 0, 0)
+vermelho_escuro = (200, 0, 0)
 cinza_claro = (128, 128, 128)
 cinza_escuro = (88, 88, 88)
 
@@ -19,19 +20,23 @@ pg.display.set_icon(icone)
 
 fonte_titulo = pg.font.SysFont("impact", 60)
 fonte_rb = pg.font.SysFont("comicsansms", 40)
+fonte_menor = pg.font.SysFont("comicsansms", 25)
+fonte_conjuntos = pg.font.SysFont("comicsansms", 18)
 
 A = ['CRUZEIRO', 'INTERNACIONAL', 'ATLETICO MINEIRO', 'VASCO DA GAMA', 'FLAMENGO']
 B = ['GREMIO', 'SAO PAULO', 'VASCO DA GAMA', 'CORINTHIANS', 'SANTOS']
-C = ['CORINTHIANS', 'SÃO PAULO', 'CRUZEIRO']
+C = ['CORINTHIANS', 'SÃO PAULO', 'CRUZEIRO', 'VASCO DA GAMA']
 
-tentativas_de_letras = [' ', '-']
+tentativas_de_letras = ['', '-']
 palavra_escolhida = ''
 palavra_camuflada = ''
 end_game = True
 chance = 0
-letra = ' '
+letra = ''
 click_last_status = False
 jogo_iniciado = False
+pergunta = ''
+conjuntos_exibidos = False
 
 def Desenho_Sair_Button(window):
     pg.draw.rect(window, vermelho, (350, 350, 300, 75))
@@ -121,30 +126,38 @@ def Desenho_Tela_Inicial(window):
 def Desenho_da_Forca(window, chance):
     imagem_fundo = pg.image.load('imagens/estadio.jpeg')
     imagem_fundo = pg.transform.scale(imagem_fundo, (1000, 600))
-
     window.blit(imagem_fundo, (0, 0))
 
     superficie_clara = pg.Surface((1000, 600))
     superficie_clara.fill((255, 255, 255))
     superficie_clara.set_alpha(100)
-
     window.blit(superficie_clara, (0, 0))
 
-    pg.draw.line(window, preto, (100, 500), (100, 100), 10)
-    pg.draw.line(window, preto, (50, 500), (150, 500), 10)
-    pg.draw.line(window, preto, (100, 100), (300, 100), 10)
-    pg.draw.line(window, preto, (300, 100), (300, 150), 10)
-    if chance >= 1: pg.draw.circle(window, preto, (300, 200), 50, 10)
-    if chance >= 2: pg.draw.line(window, preto, (300, 250), (300, 350), 10)
-    if chance >= 3: pg.draw.line(window, preto, (300, 260), (225, 350), 10)
-    if chance >= 4: pg.draw.line(window, preto, (300, 260), (375, 350), 10)
-    if chance >= 5: pg.draw.line(window, preto, (300, 350), (375, 450), 10)
-    if chance >= 6: pg.draw.line(window, preto, (300, 350), (225, 450), 10)
+    x_base = 100
+    y_base = 100
 
-def Desenho_Restart_Button(window):
-    pg.draw.rect(window, preto, (700, 100, 200, 65))
-    texto = fonte_rb.render('Restart', True, branco)
-    window.blit(texto, (740, 120))
+    tamanho_poste = 10
+    tamanho_circulo = 50
+    tamanho_linhas = 15
+    deslocamento_y = 30
+
+    altura_poste = 330
+    comprimento_barra = 220
+    comprimento_base = 140
+
+    pg.draw.line(window, preto, (x_base, y_base + altura_poste), (x_base, y_base), tamanho_poste)
+    pg.draw.line(window, preto, (x_base - comprimento_base / 2, y_base + altura_poste), 
+                 (x_base + comprimento_base / 2, y_base + altura_poste), tamanho_poste)
+    pg.draw.line(window, preto, (x_base, y_base), (x_base + comprimento_barra, y_base), tamanho_poste)
+    pg.draw.line(window, preto, (x_base + comprimento_barra, y_base), 
+                 (x_base + comprimento_barra, y_base + 60), tamanho_poste)
+
+    if chance >= 1: pg.draw.circle(window, preto, (x_base + comprimento_barra, y_base + 100), tamanho_circulo, tamanho_linhas)
+    if chance >= 2: pg.draw.line(window, preto, (x_base + comprimento_barra, y_base + 140), (x_base + comprimento_barra, y_base + 250), tamanho_linhas)
+    if chance >= 3: pg.draw.line(window, preto, (x_base + comprimento_barra, y_base + 160), (x_base + comprimento_barra - 60, y_base + 250), tamanho_linhas)
+    if chance >= 4: pg.draw.line(window, preto, (x_base + comprimento_barra, y_base + 160), (x_base + comprimento_barra + 60, y_base + 250), tamanho_linhas)
+    if chance >= 5: pg.draw.line(window, preto, (x_base + comprimento_barra, y_base + 250), (x_base + comprimento_barra + 60, y_base + 320), tamanho_linhas)
+    if chance >= 6: pg.draw.line(window, preto, (x_base + comprimento_barra, y_base + 250), (x_base + comprimento_barra - 60, y_base + 320), tamanho_linhas)
 
 def Desenho_Voltar_Menu_Button(window):
     pg.draw.rect(window, cinza_claro, (700, 175, 200, 65))
@@ -163,6 +176,105 @@ def Voltar_Ao_Menu(x, y, click, click_last_status, jogo_iniciado):
     if 700 <= x <= 900 and 175 <= y <= 240 and not click_last_status and click[0]:
         jogo_iniciado = False
     return jogo_iniciado
+
+def Mostrar_Pergunta(window, pergunta):
+    texto_pergunta = fonte_rb.render(pergunta, True, preto)
+    window.blit(texto_pergunta, (20, 20))
+
+def Desenho_Restart_Button(window):
+    pg.draw.rect(window, vermelho, (700, 100, 200, 65))
+    pg.draw.rect(window, vermelho_escuro, (700, 100, 200, 65), 5)
+
+    texto = fonte_rb.render('Restart', True, branco)
+    largura_texto = texto.get_width()
+    altura_texto = texto.get_height()
+    pos_x = (700 + 200 / 2) - largura_texto / 2
+    pos_y = (100 + 65 / 2) - altura_texto / 2
+    window.blit(texto, (pos_x, pos_y))
+
+def Restart_do_Jogo(end_game, chance, tentativas_de_letras, click_last_status, click, x, y):
+    global pergunta, palavra_escolhida
+    if 700 <= x <= 900 and 100 <= y <= 165 and not click_last_status and click[0]:
+        tentativas_de_letras = []
+        end_game = True
+        chance = 0
+        pergunta, palavra_escolhida = Sorteando_Palavra()
+    return end_game, chance, tentativas_de_letras
+
+def quebrar_texto(texto, largura_maxima, fonte):
+    palavras = texto.split(' ')
+    linhas = []
+    linha_atual = ""
+    
+    for palavra in palavras:
+        if fonte.size(linha_atual + ' ' + palavra)[0] <= largura_maxima:
+            linha_atual += (' ' + palavra) if linha_atual else palavra
+        else:
+            linhas.append(linha_atual)
+            linha_atual = palavra
+
+    if linha_atual:
+        linhas.append(linha_atual)
+    
+    return linhas
+
+def Desenho_Conjuntos(window, A, B, C):
+    conjunto_box_width = 390
+    conjunto_box_height = 240
+
+    pos_x = 600
+    pos_y = 260
+    pos_y_c = 0
+    pos_y_a = 0
+    pos_y_b = 0
+
+    conjunto_box = pg.Surface((conjunto_box_width, conjunto_box_height))
+    conjunto_box.fill((200, 200, 200))
+    conjunto_box.set_alpha(180)
+    window.blit(conjunto_box, (pos_x, pos_y))
+
+    texto_conjunto_A = f"A = [{', '.join(A)}]"
+    texto_conjunto_B = f"B = [{', '.join(B)}]"
+    texto_conjunto_C = f"C = [{', '.join(C)}]"
+
+    linhas_A = quebrar_texto(texto_conjunto_A, conjunto_box_width - 20, fonte_conjuntos)
+    linhas_B = quebrar_texto(texto_conjunto_B, conjunto_box_width - 90, fonte_conjuntos)
+    linhas_C = quebrar_texto(texto_conjunto_C, conjunto_box_width - 20, fonte_conjuntos)
+
+    margin_x = 20
+    margin_y = 10
+    gap_between_lines = 25
+
+    total_linhas_A = len(linhas_A)
+    total_linhas_B = len(linhas_B)
+    total_linhas_C = len(linhas_C)
+
+    total_altura_ocupada = (total_linhas_A + total_linhas_B + total_linhas_C) * gap_between_lines
+
+    espacamento_extra = conjunto_box_height - total_altura_ocupada
+    espacamento_entre_conjuntos = espacamento_extra // 4
+
+    pos_y_offset = pos_y + margin_y + espacamento_entre_conjuntos
+
+    pos_y_a = 270
+    for i, linha in enumerate(linhas_A):
+        window.blit(fonte_conjuntos.render(linha, True, preto), (pos_x + margin_x, pos_y_a + i * gap_between_lines))
+
+
+    pos_y_offset += total_linhas_A * gap_between_lines + espacamento_entre_conjuntos
+    pos_y_b = 353
+    for i, linha in enumerate(linhas_B):
+        window.blit(fonte_conjuntos.render(linha, True, preto), (pos_x + margin_x, pos_y_b + i * gap_between_lines))
+
+    pos_y_offset += total_linhas_B * gap_between_lines + espacamento_entre_conjuntos
+    pos_y_c += 440
+    for i, linha in enumerate(linhas_C):
+        window.blit(fonte_conjuntos.render(linha, True, preto), (pos_x + margin_x, pos_y_c + i * gap_between_lines))
+
+    pos_y_final = pos_y + conjunto_box_height - margin_y - gap_between_lines
+    if pos_y_offset + (total_linhas_C * gap_between_lines) < pos_y_final:
+        espacamento_final = pos_y_final - (pos_y_offset + (total_linhas_C * gap_between_lines))
+        pos_y_offset += espacamento_final
 
 def Sorteando_Palavra(times, end_game):
     if end_game:
@@ -189,7 +301,7 @@ def Tentando_uma_Letra(tentativas_de_letras, palavra_escolhida, letra, chance):
 
 def Palavra_do_Jogo(window, palavra_camuflada):
     palavra = fonte_rb.render(palavra_camuflada, True, preto)
-    window.blit(palavra, (200, 500))
+    window.blit(palavra, (30, 460))
 
 def Restart_do_Jogo(end_game, chance, tentativas_de_letras, click_last_status, click, x, y):
     if 700 <= x <= 900 and 100 <= y <= 165 and not click_last_status and click[0]:
@@ -197,6 +309,11 @@ def Restart_do_Jogo(end_game, chance, tentativas_de_letras, click_last_status, c
         end_game = True
         chance = 0
     return end_game, chance, tentativas_de_letras
+
+def Desenho_Letras_Tentadas(window, tentativas_de_letras):
+    letras = 'Letras tentadas: ' + ', '.join(tentativas_de_letras)
+    texto_tentativas = fonte_menor.render(letras, True, preto)
+    window.blit(texto_tentativas, (20, 550))
 
 while True:
     for event in pg.event.get():
@@ -225,6 +342,8 @@ while True:
         Desenho_da_Forca(window, chance)
         Desenho_Restart_Button(window)
         Desenho_Voltar_Menu_Button(window)
+        Desenho_Letras_Tentadas(window, tentativas_de_letras)
+        Mostrar_Pergunta(window, pergunta)
 
         if end_game:
             palavra_escolhida, end_game = Sorteando_Palavra(A, end_game)
@@ -232,6 +351,11 @@ while True:
 
         palavra_camuflada = Camuflando_Palavra(palavra_escolhida, tentativas_de_letras)
         Palavra_do_Jogo(window, palavra_camuflada)
+
+        Desenho_Conjuntos(window, 
+                         ['CRUZEIRO', 'INTERNACIONAL', 'ATLETICO MINEIRO', 'VASCO DA GAMA', 'FLAMENGO'],
+                         ['GREMIO', 'SAO PAULO', 'VASCO DA GAMA', 'CORINTHIANS', 'SANTOS'],
+                         ['CORINTHIANS', 'SÃO PAULO', 'CRUZEIRO', 'VASCO DA GAMA'])
 
         end_game, chance, tentativas_de_letras = Restart_do_Jogo(end_game, chance, tentativas_de_letras, click_last_status, click, x, y)
 
